eb7d00ef76b2b9e5ce4c9d71939e539693180dd6
gitpk-0 <kell108p@protonmail.com>
2022-06-27 09:30:01 -0400

    second

---
+++ HelloWorld1/src/main/java/pk/helloworld1/HelloWorld1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package pk.helloworld1;
+
+/**
+ *
+ * @author pk
+ */
+public class HelloWorld1 {
+
+    public static void main(String[] args) {
+        // Print the words Hello World on the screen
+        System.out.println("Hello World");
+    }
+}
---
+++ eclipse/tutorial/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author pk
+ *
+ */
+module tutorial {
+}
