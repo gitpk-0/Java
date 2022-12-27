2f62da4a3a980d974b47ee63e145101954a9cc98
gitpk-0 <kell108p@protonmail.com>
2022-12-27 11:22:30 -0500

    room & dog

--- java_mooc/ClassesAndObjects.java
+++ java_mooc/ClassesAndObjects.java
@@ -64,3 +64,53 @@ public class Person {. Each variable is preceded by the keyword private. The
 keyword private means that the variables are "hidden" inside the object. This 
 is known as encapsulation.
 **/
+
+
+// Defining a Constructor
+// Objects are always created using a constructor.
+
+public static void main(String[] args) {
+    Person ada = new Person("Ada");
+    // ...
+}
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String initialName) {
+        this.age = 0;
+        this.name = initialName;
+    }
+}
+
+// The constructor's name is always the same as the class name.
+
+// Default Constructor
+// If the programmer does not define a constructor for a class, Java 
+// automatically creates a default one for it. A default constructor is a 
+// constructor that doesn't do anything apart from creating the object. The 
+// object's variables remain uninitialized (generally, the value of any object 
+//     references will be null, meaning that they do not point to anything, and 
+//     the values of primitives will be 0)
+
+// For example, an object can be created from the class below by making the 
+// call new Person()
+
+public class Person {
+    private String name;
+    private int age;
+}
+// If a constructor has been defined for a class, no default constructor exists.
+ // For the class below, calling new Person would cause an error, as Java cannot 
+ // find a constructor in the class that has no parameters.
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String initialName) {
+        this.age = 0;
+        this.name = initialName;
+    }
+}
