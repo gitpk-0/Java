2b4692d34f50dbd6519d75a0e057373db9a9fb08
gitpk-0 <kell108p@protonmail.com>
2023-01-02 12:16:13 -0500

    items

--- java_mooc/ClassesAndObjects.java
+++ java_mooc/ClassesAndObjects.java
@@ -306,3 +306,32 @@ public String toString() {
     return this.name + ", age " + this.age + " years, my BMI is " + bodyMassIndex();
 } // no emphasis is placed on the fact that the object's own method is being called
 
+
+
+/* Adding objects to a list: */
+
+// Creating a list meant for storing Person type objects
+
+ArrayList<Person> persons = new ArrayList<>();
+
+// a person object can be created first
+Person john = new Person("John");
+// and then added to the list
+persons.add(john);
+
+// person objects can also be created "in the same sentence" that they are 
+//   added to the list
+persons.add(new Person("Matthew"));
+persons.add(new Person("Martin"));
+
+for (Person person: persons) {
+    System.out.println(person);
+}
+// Output
+// John, age 0 years
+// Matthew, age 0 years
+// Martin, age 0 years
+
+
+
+/* Multiple constructor parameters */
