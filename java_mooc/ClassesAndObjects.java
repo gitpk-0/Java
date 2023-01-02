86f8259e743cb7ee7f9a4c3bbb7befabe95bc809
gitpk-0 <kell108p@protonmail.com>
2023-01-02 13:58:01 -0500

    personalInformation

--- java_mooc/ClassesAndObjects.java
+++ java_mooc/ClassesAndObjects.java
@@ -335,3 +335,20 @@ for (Person person: persons) {
 
 
 /* Multiple constructor parameters */
+
+public class Person {
+
+    private String name;
+    private int age;
+    private int weight;
+    private int height;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.weight = 0;
+        this.height = 0;
+    }
+
+    // methods
+}
