b26c3edcc4dc264bdd494bb08729344c1a009bb4
gitpk-0 <kell108p@protonmail.com>
2023-01-16 13:07:37 -0500

    messaging service

--- java_mooc/Notes/Objects&Lists.java
+++ java_mooc/Notes/Objects&Lists.java
@@ -31,3 +31,81 @@ list.printSongs();
 // Sorateiden kuningas
 // Teuvo, maanteiden kuningas
 
+
+
+/* Objects in an Instance Variable List */
+
+/* A list that is an object's instance variable can contain objects other than 
+strings as long as the type of objects in the list is specified when defining 
+the list. */
+
+public class AmusementParkRide {
+    private String name;
+    private int minimumHeight;
+    private int visitors;
+
+    public AmusementParkRide(String name, int minimumHeight) {
+        this.name = name;
+        this.minimumHeight = minimumHeight;
+        this.visitors = 0;
+    }
+
+    public boolean isAllowedOn(Person person) {
+        if (person.getHeight() < this.minimumHeight) {
+            return false;
+        }
+
+        this.visitors++;
+        return true;
+    }
+
+    public String toString() {
+        return this.name + ", minimum height requirement: " + this.minimumHeight +
+            ", visitors: " + this.visitors;
+    }
+}
+
+public class AmusementParkRide {
+    private String name;
+    private int minimumHeigth;
+    private int visitors;
+    private ArrayList<Person> riding;
+
+    public AmusementParkRide(String name, int minimumHeigth) {
+        this.name = name;
+        this.minimumHeigth = minimumHeigth;
+        this.visitors = 0;
+        this.riding = new ArrayList<>();
+    }
+
+    // ..
+}
+
+public class AmusementParkRide {
+    private String name;
+    private int minimumHeight;
+    private int visitors;
+    private ArrayList<Person> riding;
+
+    public AmusementParkRide(String name, int minimumHeight) {
+        this.name = name;
+        this.minimumHeight = minimumHeight;
+        this.visitors = 0;
+        this.riding = new ArrayList<>();
+    }
+
+    public boolean isAllowedOn(Person person) {
+        if (person.getHeight() < this.minimumHeight) {
+            return false;
+        }
+
+        this.visitors++;
+        this.riding.add(person);
+        return true;
+    }
+
+    public String toString() {
+        return this.name + ", minimum height requirement: " + this.minimumHeight +
+            ", visitors: " + this.visitors;
+    }
+}
