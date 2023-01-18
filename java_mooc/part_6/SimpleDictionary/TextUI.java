53d6e7a926b66b480456869205276ce3b4790f85
gitpk-0 <kell108p@protonmail.com>
2023-01-17 19:22:26 -0500

    simpledictionary

---
+++ java_mooc/part_6/SimpleDictionary/TextUI.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class TextUI {
+    private Scanner scanner;
+    private SimpleDictionary dictionary;
+
+
+    public TextUI(Scanner scanner, SimpleDictionary dictionary) {
+        this.scanner = scanner;
+        this.dictionary = dictionary;
+    }
+
+    public void start() {
+
+        while (true) {
+            System.out.println("Command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("end")) {
+                System.out.println("Bye bye!");
+                break;
+            }
+
+            if (command.equals("add")) {
+                System.out.println("Word: ");
+                String word = scanner.nextLine();
+
+                System.out.println("Translation: ");
+                String translation = scanner.nextLine();
+
+                // add word and translation to dictionary
+                dictionary.add(word, translation);
+            }
+
+            if (command.equals("search")) {
+                System.out.println("To be translated: ");
+                String word = scanner.nextLine();
+
+                if (dictionary.wordInDict(word)) {
+                    System.out.println("Translation: " + dictionary.translate(word));
+                } else {
+                    System.out.println("Word " + word + " was not found");
+                }
+            }
+
+            if (!(command.equals("end") || command.equals("add") || command.equals("search"))) {
+                System.out.println("Unknown command");
+            }
+        }
+    }
+}
