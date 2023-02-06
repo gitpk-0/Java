bce005f0a696d6e689829b574b0c92c088a700bf
gitpk-0 <kell108p@protonmail.com>
2023-02-06 10:54:51 -0500

    onlineShop

---
+++ java_mooc/part_9/OnlineShop/ShoppingCart.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingCart {
+
+    private Map<String, Item> cart;
+
+    public ShoppingCart() {
+        this.cart = new HashMap<>();
+    }
+
+    public void add(String product, int price) {
+        if (cart.containsKey(product)) {
+            cart.get(product).increaseQuantity();
+            return;
+        }
+        cart.put(product, new Item(product, 1, price));
+    }
+
+    public int price() {
+        int total = 0;
+        for (String item : this.cart.keySet()) {
+            total += cart.get(item).price();
+        }
+        return total;
+    }
+
+    public void print() {
+        for (String item : this.cart.keySet()) {
+            System.out.println(this.cart.get(item));
+        }
+    }
+}
